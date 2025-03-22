4f6573f76c8c87d60439c87cfa8bfd6655aeecd7
zaheerb <zaheero2312@gmail.com>
2025-03-22 13:49:10 +0200

    first commit

---
+++ calculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        double number1;
+        double number2;
+        char operator;
+        double result;
+        boolean validOperator = true;
+        
+        System.out.print("Enter the first number: ");
+        number1 = scanner.nextDouble();
+
+        System.out.print("Enter the operator: ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        number2 = scanner.nextDouble();
+
+        switch(operator){
+            case '+' -> result = number1 + number2;
+            case '-' -> result = number1 - number2;
+            case '*' -> result = number1 * number2;
+            case '/' -> {
+                if(number2 == 0) {
+                    System.out.println("Cannot divide by zero.");
+                    validOperator = false;
+                }
+                else{
+                    result = number1 / number2;
+                }
+            }
+            case '^' -> result = Math.pow(number1, number2);
+            default -> {
+                System.out.println("That is an invalid operator");
+                validOperator = false;
+            }
+
+        }
+
+        scanner.close();
+
+    }
+}
