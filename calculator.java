b42609c061df02eff5c4e1f086e0cdc235e03328
zaheerb <zaheero2312@gmail.com>
2025-03-23 22:20:35 +0200

    Changed following: - Added comments - Added logic to check for incorrect operator the moment it is selected - Removed the initial incorrect operator checker - Removed boolean variable and entries as they were not being used - Printing result without scientific notations

--- calculator.java
+++ calculator.java
@@ -1,3 +1,4 @@
+// Importing function for user input
 import java.util.Scanner;
 
 public class calculator {
@@ -6,44 +7,52 @@ public class calculator {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Variables
         double number1;
         double number2;
         char operator;
-        double result;
-        boolean validOperator = true;
-        
+        double result = 0;
+
+        // Requesting first number from user
         System.out.print("Enter the first number: ");
         number1 = scanner.nextDouble();
 
+        // Requesting operator from user
         System.out.print("Enter the operator: ");
         operator = scanner.next().charAt(0);
 
+        // Checking if user selected a valid operator before result
+        if (operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '^') {
+            System.out.println("That is not a valid operator.");
+            return;  // Exit the program
+        }
+
+        // Requesting second number from user
         System.out.print("Enter the second number: ");
         number2 = scanner.nextDouble();
 
+        // Performs operations based on selected operator
         switch(operator){
-            case '+' -> result = number1 + number2;
-            case '-' -> result = number1 - number2;
-            case '*' -> result = number1 * number2;
-            case '/' -> {
+            case '+' -> result = number1 + number2; // Addition
+            case '-' -> result = number1 - number2; // Subtraction
+            case '*' -> result = number1 * number2; // Multiplication
+            case '/' -> { // Division | Cannot divide by zero
                 if(number2 == 0) {
                     System.out.println("Cannot divide by zero.");
-                    validOperator = false;
+                    return;
                 }
                 else{
                     result = number1 / number2;
                 }
             }
             case '^' -> result = Math.pow(number1, number2);
-            default -> {
-                System.out.println("That is an invalid operator");
-                validOperator = false;
+
             }
 
-        }
+        // Print result without scientific notation
+        System.out.printf("Your result is %f%n", result);
 
         scanner.close();
-
 
     }
 }
