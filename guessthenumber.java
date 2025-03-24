00d3060a3a56b33e319d3177d69d2862db6a995b
zaheerb <zaheero2312@gmail.com>
2025-03-24 19:10:36 +0200

    Update:
    - Added comments
    - "Guess a number between..." needed to be placed onto a new line as the word formatting came out wrong
    -

--- guessthenumber.java
+++ guessthenumber.java
@@ -1,4 +1,7 @@
+// Importing random class
 import java.util.Random;
+
+// Importing scanner class
 import java.util.Scanner;
 
 public class guessthenumber {
@@ -7,6 +10,7 @@ public class guessthenumber {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
+        // Variables
         int guess;
         int attempts = 0;
         int min = 1;
@@ -14,21 +18,26 @@ public class guessthenumber {
         int randomNumber = random.nextInt(min, max + 1);
 
         System.out.println("Welcome to the number guessing game!");
-        System.out.printf("Guess a number between %d-%d", min, max);
+
+        // Print instruction to guess between min and max variable values
+        System.out.printf("Guess a number between %d-%d\n", min, max);
 
         do{
-            System.out.print("Guess the number: ");
-            guess = scanner.nextInt();
-            attempts++;
+            System.out.print("Enter a guess: ");
+            guess = scanner.nextInt(); // User to input their guess
+            attempts++; // Adds total attempts until correct answer is given
 
+            // Hint to user that the guess is less than the random number
             if(guess < randomNumber){
                 System.out.println("Your guess is too low. Try again");
             }
 
+            // Hint to user that the guess is more than the random number
             else if(guess > randomNumber){
                 System.out.println("Your guess is too high. Try again");
             }
 
+            // Prints success and number of attempts it took
             else{
                 System.out.println("You're correct! Well done! The number was " + randomNumber);
                 System.out.println("It took you " + attempts + " attempts");
